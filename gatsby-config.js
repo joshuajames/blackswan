99729f0649c4128e6cd7e8616700fce0cecbdd98
Joshua J Arnold <joshua@blackswanfarming.com>
2021-02-27 22:13:09 +1300

    update icon set and naming in config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,13 +62,13 @@ module.exports = {
       // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter WordPress Blog`,
-        short_name: `GatsbyJS & WP`,
+        name: `Black Swan Farming`,
+        short_name: `BSF`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/static/android-chrome-512x512.png`,
       },
     },
 
