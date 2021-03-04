5fd1997eeaa4fabdbde9f14554811e26754a330d
Joshua J Arnold <joshua@blackswanfarming.com>
2021-03-04 15:29:47 +1300

    develop

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -3,7 +3,7 @@ module.exports = [{
       options: {"plugins":[]},
     },{
       plugin: require('../node_modules/gatsby-source-wordpress/gatsby-browser.js'),
-      options: {"plugins":[],"url":"https://blackswanfarming.com/graphql","schema":{"requestConcurrency":0,"previewRequestConcurrency":0,"queryDepth":15,"circularQueryLimit":5,"typePrefix":"Wp","timeout":30000,"perPage":100},"verbose":true},
+      options: {"plugins":[],"url":"https://blackswanfarming.com/graphql","schema":{"requestConcurrency":0,"previewRequestConcurrency":0,"timeout":40000,"queryDepth":15,"circularQueryLimit":5,"typePrefix":"Wp","perPage":100},"verbose":true},
     },{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"Gatsby Starter WordPress Blog","short_name":"GatsbyJS & WP","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"content/assets/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"92ae69ec4a53b36d601a86151c6941b5"},
